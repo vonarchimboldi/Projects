1513f6b7bae3c827be3ad8d2717077f11f29cbb9
Abhinav Katoch <abhinavkatoch@Abhinavs-MBP.attlocal.net>
2020-02-19 18:57:19 -0600

    changed file  structure

--- dlframework/models/fastaitabular.py
+++
@@ -1,7 +0,0 @@
-from fastai.tabular import *
-
-
-learn = tabular_learner(data, layers=[200,100], emb_szs={'native-country': 10}, metrics=accuracy)
-learn.fit_one_cycle(1, 1e-2)
-
-print(learn.predict(df.iloc[0]))
