22b65cbece0eac8155aa6f358816beec796b1416
Abhinav Katoch <abhinavkatoch@Abhinavs-MBP.attlocal.net>
2020-02-21 13:36:02 -0600

    more flesh

---
+++ dlframework/src/intervals.py
@@ -0,0 +1,52 @@
+from nonconformist.cp import IcpClassifier, IcpRegressor
+from nonconformist.nc import NcFactory
+import pandas as pd
+import numpy as np
+import joblib
+import os
+
+TRAINING = os.environ.get("TRAINING_DATA")
+CALIBRATION = os.environ.get("CALIBRATION_SET")
+TEST = os.environ.get("TEST_DATA")
+
+df_train = pd.read_csv(TRAINING)
+df_cal = pd.read_csv(CALIBRATION)
+df_test = pd.read_csv(TEST)
+trainX, trainY = df_train.drop(['TARGET'],  axis = 1), df_train['TARGET']
+calX, calY = df_cal.drop(['TARGET'], axis = 1), df_cal['TARGET']
+
+if 'TARGET' in df_test.columns:
+    testX, testY = df_test.drop(['TARGET'], axis = 1), df_test['TARGET']
+else:
+    testX = df_test
+    
+if PROBLEM_TYPE == 'classification':
+        
+    nc = NcFactory.create_nc(model, normalizer_model=KNeighborsRegressor(n_neighbors=11))	# Create a default nonconformity function
+    icp = IcpClassifier(nc)
+
+    icp.fit(trainX, trainY)
+
+    # Calibrate the ICP using the calibration set
+    icp.calibrate(calX, calY)
+
+    # Produce predictions for the test set, with confidence 95%
+    prediction = icp.predict(testX.to_numpy(), significance=0.05)
+
+
+else:
+
+    nc = NcFactory.create_nc(model, normalizer_model=KNeighborsRegressor(n_neighbors=11))	# Create a default nonconformity function
+    icp = IcpRegressor(nc)			# Create an inductive conformal regressor
+
+    # Fit the ICP using the proper training set
+    icp.fit(trainX, trainY)
+
+    # Calibrate the ICP using the calibration set
+    icp.calibrate(calX, calY)
+
+    # Produce predictions for the test set, with confidence 95%
+    prediction = icp.predict(testX.to_numpy(), significance=0.05)
+
+sub = pd.DataFrame(prediction, columns = ['lower_bound', 'upper_bound'])
+sub.to_csv(f"{PROBLEM_TYPE}_{MODEL}_{DATASET}_intervals.csv", index = False)
---
+++ dlframework/src/predict.py
@@ -0,0 +1,27 @@
+import os
+from xgboost import XGBClassifier, XGBRegressor
+from lightgbm as LGBMClassifier, LGBMRegressor
+from catboost import CatBoostClassifier, CatBoostRegressor
+
+TESTING = os.environ.get("TEST_DATA")
+MODEL = os.environ.get("MODEL")
+PROBLEM_TYPE = os.environ.get("PROBLEM_TYPE")
+DATASET = os.environ.get("DATASET")
+
+df_test = pd.read_csv(TESTING)
+
+if 'TARGET' in df_test.columns:
+    actuals = df_test['TARGET']
+    df_test = df_test.drop(['TARGET'], axis = 1)
+else:
+    actuals = np.zeros(len(df_test.index))
+
+model = joblib.load(os.path.join("models", f"{MODEL}.pkl"))
+
+if PROBLEM_TYPE == 'regression':
+    preds = model.predict(df_test)
+elif PROBLEM_TYPE == 'classification':
+    preds = model.predict_proba(df_test)[0]
+
+sub = pd.DataFrame(np.columnstack((preds, actuals)), columns = ['predictions', 'actuals'])
+sub.to_csv(f'{PROBLEM_TYPE}_{MODEL}_{DATASET}_outofsample.csv', index = False)
